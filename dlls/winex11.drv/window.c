2785ef097c913b7928dc15d6dd3345b28a8a516c
Rémi Bernon <rbernon@codeweavers.com>
2026-06-30 09:08:43 +0200

    winex11: Use the desired state for mapping delays.
    This matches the state the host window is supposed to be in, whereas
    pending_state is the state we have requested yet but which may need
    further requests.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59932
    (cherry picked from commit 2b05f63811f0e1acfd13e0f61d1a668b32a6de9a)

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -3725,7 +3725,7 @@ void X11DRV_WindowPosChanged( HWND hwnd, HWND insert_after, HWND owner_hint, UIN
     }
 
     /* visible windows are only hidden after SWP_HIDEWINDOW is used */
-    if (data->pending_state.wm_state != WithdrawnState && !(new_style & WS_VISIBLE) &&
+    if (data->desired_state.wm_state != WithdrawnState && !(new_style & WS_VISIBLE) &&
         !(swp_flags & SWP_HIDEWINDOW))
     {
         WARN( "win %p/%lx not yet hidden, delaying unmapping\n", hwnd, data->whole_window );
@@ -3733,7 +3733,7 @@ void X11DRV_WindowPosChanged( HWND hwnd, HWND insert_after, HWND owner_hint, UIN
     }
 
     /* layered windows are mapped only once their attributes are set */
-    if (data->pending_state.wm_state == WithdrawnState && (new_style & WS_VISIBLE) &&
+    if (data->desired_state.wm_state == WithdrawnState && (new_style & WS_VISIBLE) &&
         (ex_style & WS_EX_LAYERED) && !data->layered && !IsRectEmpty( &new_rects->window ))
     {
         WARN( "win %p/%lx is layered, delaying mapping\n", hwnd, data->whole_window );
